4b11da6dd396fea1db77fd448b5abcbd72b3800d
Gazi <gazideniz.90@gmial.com>
2025-11-05 09:34:23 +0300

    diziler ders notları eklendi

---
+++ diziler/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] hayvanlar = new string[5] { "Kedi", "Köpek", "Tavşan", "Kuğu", "At" };
+            int[] dizi;
+            dizi = new int[5];
+            string[] renkler = new string[5];
+            renkler[1] = "Mavi";
+            dizi[3] = 10;
+
+            Console.WriteLine(hayvanlar[2]);
+            Console.WriteLine(renkler[1]);
+            Console.WriteLine(dizi[3]);
+
+            Console.WriteLine("Lütfen dizinin eleman sayısını giriniz:");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi = new int[diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.WriteLine("Lütfen {0}. sayıyı giriniz:", i + 1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            }
+            int toplam = 0;
+            foreach (var sayi in sayiDizisi)
+            {
+                toplam += sayi;
+            }
+            Console.WriteLine("Dizinin elemanlarının toplamı: " + toplam);
+        }
+    }
+}

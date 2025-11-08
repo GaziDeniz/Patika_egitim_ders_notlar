265ca9e11d0115fc37c8df90ad10cbfafd639dc4
Gazi <gazideniz.90@gmial.com>
2025-11-08 17:51:16 +0300

    method overloading ders notları eklendi

---
+++ metotOverloading/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace metotOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sonuc1 = Topla(4, 5);
+            Console.WriteLine("Topla(int, int): " + sonuc1);
+
+            int sonuc2 = Topla(4, 5, 6);
+            Console.WriteLine("Topla(int, int, int): " + sonuc2);
+
+            double sonuc3 = Topla(4.5, 5.5);
+            Console.WriteLine("Topla(double, double): " + sonuc3);
+        }
+
+        static int Topla(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Topla(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        static double Topla(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
